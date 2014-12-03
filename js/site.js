0a8269117751cd295bb390640ae9769091f2f7b6
Rees Retuta <rees.retuta@gmail.com>
2014-12-02 17:08:17 -0800

    update

--- js/site.js
+++ js/site.js
@@ -18,21 +18,6 @@ $(document).ready(function() {
 
 
 
-			var clock;
-			// Grab the current date
-			var currentDate = new Date();
-
-			// Set some date in the future. In this case, it's always Jan 1
-			var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
-
-			// Calculate the difference in seconds between the future and current date
-			var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-
-			// Instantiate a coutdown FlipClock
-			clock = $('.clock').FlipClock(diff, {
-				clockFace: 'DailyCounter',
-				countdown: true
-			});
             
 });
 
@@ -68,4 +53,20 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 
 myApp.controller('homeController',function($rootScope, $scope, $stateParams){
     // $("#flyer-wrapper").slideDown(3000);
+    // 
+			var clock;
+			// Grab the current date
+			var currentDate = new Date();
+
+			// Set some date in the future. In this case, it's always Jan 1
+			var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+
+			// Calculate the difference in seconds between the future and current date
+			var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+			// Instantiate a coutdown FlipClock
+			clock = $('.clock').FlipClock(diff, {
+				clockFace: 'DailyCounter',
+				countdown: true
+			});
 });
