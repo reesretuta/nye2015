5aeb516777f869a0063719781dbebe3eb47954b3
Rees Retuta <rees.retuta@gmail.com>
2014-12-08 20:20:12 -0800

    analytics

--- js/site.js
+++ js/site.js
@@ -98,3 +98,23 @@ myApp.controller('homeController',function($rootScope, $scope, $stateParams){
 				countdown: true
 			});
 });
+
+
+myApp.run(function($rootScope, $state, $stateParams, $window, $location){
+        $rootScope.$on('$stateChangeSuccess', function(event){
+            if (!$window.ga) {
+                return;
+            }
+            $window.ga('send', 'pageview', { page: $location.path(), title: $state.current.name});
+            // $window.ga('send', 'pageview', { page: $location.path(), title: $state.current.title });
+        });
+
+});
+
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+//
+// ga('create', 'UA-2834106-3', 'auto');
+// ga('send', 'pageview');
